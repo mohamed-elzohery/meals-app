294f1447b58ce035b582648d78db200f2e5fa6d9
mohamed-elzohery <m.zohery1998@gmail.com>
2022-06-21 21:29:50 +0200

    Adding side drawer

--- App.js
+++ App.js
@@ -3,13 +3,43 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 //  Screens
 import CategoryScreen from './screens/CategoryScreen';
 import MealsOverview from './screens/MealsOverview';
 import MealDetails from './screens/MealDetails';
+import FavouritesScreen from './screens/FavouritesScreen';
 
+//  Navigatord
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return <Drawer.Navigator screenOptions={
+    {
+      drawerActiveBackgroundColor: "#b9ffb0",
+      drawerActiveTintColor: '#3f2f25',
+      drawerInactiveTintColor: '#FFF',
+      headerStyle: {backgroundColor: '#351401'},
+      headerTintColor: '#FFF',
+      sceneContainerStyle: {backgroundColor: '#3f2f25'},
+      drawerContentStyle: {backgroundColor: '#3f2f25'}
+    }
+  }>
+    <Drawer.Screen name='AllCategories' component={CategoryScreen} options={{
+      title: "All Categories",
+      drawerIcon: (({color, size}) => <Icon name="list" color={color} size={size}/>)
+    }}/>
+    <Drawer.Screen name='Favorites' component={FavouritesScreen} 
+    options={{
+      title: "Favorites",
+      drawerIcon: (({color, size}) => <Icon name="star" color={color} size={size}/>)
+    }}
+    />
+  </Drawer.Navigator>
+}
 
 const screenStyle = {
   headerStyle: {
@@ -27,8 +57,8 @@ export default function App() {
     <StatusBar barStyle="dark-content" backgroundColor='#351401' />
     <NavigationContainer>
     <Stack.Navigator screenOptions={screenStyle}>
-      <Stack.Screen name='MealsCategories' component={CategoryScreen} 
-      options={{title: "All Categories"}} />
+      <Stack.Screen name='MealsCategories' component={DrawerNavigator} 
+      options={{headerShown: false}} />
       <Stack.Screen name='MealsOverview' component={MealsOverview} />
       <Stack.Screen name="MealDetails" component={MealDetails} />
     </Stack.Navigator>
---
+++ screens/FavouritesScreen.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+const FavouritesScreen = () => {
+    return <Text>fvdfvfdvdf</Text>
+}
+
+
+export default FavouritesScreen;
